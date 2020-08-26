a65d7c9446c9c50ebb6a765cfc8e8200d04d559b
Michael Spengler <mycamper@gmx.de>
2020-08-26 21:27:45 +0200

    unit test

--- test.ts
+++ test.ts
@@ -1,15 +1,17 @@
 import { fail, assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { Web3Eth } from "./web3-eth.ts";
+import { Web3 } from "./web3.ts";
+import { Helper } from "./helper.ts";
 
 
 Deno.test("get balance", async (): Promise<void> => {
 
-    try {
-        const exampleAddress = '0x....'
-        Web3Eth.getBalance(exampleAddress)
-        fail('An error was expected as implementation has not begun yet.')
-    } catch(error){
-        assertEquals('Method needs to be implemented', error.message)
+    const exampleAddress = '0x742d35cc6634c0532925a3b844bc454e4438f44e'
+    const balance = Helper.getEthFromWei(await Web3.getBalance(exampleAddress))
+    
+    console.log(`balance in Eth: ${balance}`)
+
+    if (balance < 100000){
+        fail(`it seems unlikely that the balance of ${balance} is correct.`)
     }
 
 });
