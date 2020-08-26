d8884e67bb926f3b804a8e4868413c9a6820861a
Michael Spengler <mycamper@gmx.de>
2020-08-26 21:37:40 +0200

    minimal viable scope for my hobby projects :)

--- web3-server/web3-server.js
+++ web3-server/web3-server.js
@@ -1,12 +1,8 @@
 
-// https://ethereum.stackexchange.com/questions/42929/send-payment-from-wallet-using-web3
-// https://web3js.readthedocs.io/en/v1.2.7/web3-utils.html#tohex
-
 
 const fs = require('fs-sync')
 const path = require('path')
 const express = require('express')
-const axios = require('axios')
 const Web3 = require('web3');
 const Tx = require('ethereumjs-tx').Transaction;
 const http = require('http')
@@ -17,7 +13,6 @@ const configFileId = path.join(path.resolve(''), './web3-server/.env.json')
 const config = fs.readJSON(configFileId)
 
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${config.infuraProjectId}`));
-// let web3 = new Web3(Web3.givenProvider)
 
 executeMasterplan()
     .then((result) => {
@@ -35,17 +30,7 @@ async function executeMasterplan() {
 function defineRoutes(app) {
 
     app.get('/sendSignedTransaction', async (req, res) => {
-        // var privateKey = Buffer.from('e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109', 'hex');
         var privateKey = Buffer.from(req.query.privateKey, 'hex');
-
-        // var rawTx = {
-        //     nonce: '0x00',
-        //     gasPrice: '0x09184e72a000',
-        //     gasLimit: '0x2710',
-        //     to: '0x0000000000000000000000000000000000000000',
-        //     value: '0x00',
-        //     data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057'
-        // }
 
         var nonce = await web3.eth.getTransactionCount(req.query.fromAddress);
         console.log(`\n\nnonce: ${nonce}`)
@@ -59,11 +44,6 @@ function defineRoutes(app) {
         const myAmount = web3.utils.toHex(req.query.amountToBeSent)
         console.log(`\n\nmyAmount: ${myAmount}`)
         
-    // console.log(amountToBeSent)
-
-    // const rawTransaction = {
-    //     "from": fromAddress,
-    //     "nonce": web3.utils.toHex(nonce),
         var rawTx = {
             nonce,
             gasPrice: myGasPrice,
@@ -71,18 +51,13 @@ function defineRoutes(app) {
             fromAddress: req.query.fromAddress,
             to: req.query.toAddress,
             value: myAmount,
-            // data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057'
         }
 
-        // var tx = new Tx(rawTx, { 'chain': 'mainnet' });
         var tx = new Tx(rawTx);
 
         tx.sign(privateKey);
 
         var serializedTx = tx.serialize();
-
-        console.log(serializedTx.toString('hex'));
-        // 0xf889808609184e72a00082271094000000000000000000000000000000000000000080a47f74657374320000000000000000000000000000000000000000000000000000006000571ca08a8bbf888cfa37bbf0bb965423625641fc956967b81d12e23709cead01446075a01ce999b56a8a88504be365442ea61239198e23d1fce7d00fcfc5cd3b44b7215f
 
         web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
             .on('receipt', console.log);
@@ -116,46 +91,4 @@ function startListening(app) {
         httpServer.listen(config.httpPort)
         console.log(`listening on : http://localhost:${config.httpPort}`)
     }
-}
-
-
-async function transfer(fromAddress, toAddress, amountToBeSent, privateKey, chainId = 4) {
-
-    console.log(`preparing transaction with \n${fromAddress} \n${toAddress} \n${amountToBeSent} \n${privateKey} \n${chainId}`)
-
-    const EthereumTx = require('ethereumjs-tx').Transaction
-    const privateKeyBuffer = Buffer.from(privateKey, 'hex')
-
-    var nonce = web3.eth.getTransactionCount(fromAddress);
-
-    const myGasPrice = (await web3.eth.getGasPrice()) * 10
-
-    console.log(amountToBeSent)
-
-    const rawTransaction = {
-        "from": fromAddress,
-        "nonce": web3.utils.toHex(nonce),
-        "gasPrice": web3.utils.toHex(myGasPrice),
-        "gasLimit": web3.utils.toHex(33000),
-        // "gasLimit": '0x2710',
-        "to": toAddress,
-        "value": amountToBeSent,
-        // "chainId": chainId //remember to change this
-    };
-
-    const tx = new EthereumTx(rawTransaction)
-    tx.sign(privateKeyBuffer)
-
-    const serializedTx = tx.serialize()
-
-    console.log(`this is getting serious :)`)
-
-    web3.eth.sendTransaction('0x' + serializedTx.toString('hex'), function (err, hash) {
-        if (!err) {
-            console.log('Txn Sent and hash is ' + hash);
-        }
-        else {
-            console.error(err);
-        }
-    });
 }
