018c2d8be158b86c88b1df4fc9c6f31b388bba8a
Michael Spengler <mycamper@gmx.de>
2020-08-28 21:25:31 +0200

    boy scout rule

--- examples/get-balance.ts
+++ examples/get-balance.ts
@@ -1,7 +1,7 @@
 import * as log from "https://deno.land/std/log/mod.ts";
-
 import { Web3 } from "../web3.ts";
 import { fromAddress } from "./.env.ts";
+import { Helper } from "../helper.ts";
 
-const balance = (await Web3.getBalance(fromAddress)) / 1000000000000000000
+const balance = Helper.getEthFromWei(await Web3.getBalance(fromAddress))
 log.info(balance)
