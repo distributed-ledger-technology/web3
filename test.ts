46b1557af0b0db2103ede95bf19e3b9af0a1a48f
Michael <michael.spengler@sap.com>
2021-11-03 22:26:40 +0100

    boyscout rule

--- test.ts
+++
@@ -1,8 +0,0 @@
-import Web3 from 'https://deno.land/x/web3/mod.ts'
-import { PROVIDER_URL } from './.env.ts'
-
-const web3 = new Web3(new (Web3 as any).providers.HttpProvider(PROVIDER_URL))
-
-const balance = await (web3 as any).eth.getBalance("0x7a915e362353d72570dcf90aa5baa1c5b341c7aa")
-
-console.log(`the balance is ${balance} wei`)
