37ac075638589681d604001fea484de74ae4ca06
Oli <84723965+o-ebit@users.noreply.github.com>
2023-11-08 18:29:12 +0000

    Update import locations

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
-// @deno-types="https://raw.githubusercontent.com/o-ebit/web3/v1.0.0-myfork/types/index.d.ts"
+// @deno-types="https://raw.githubusercontent.com/o-ebit/web3/v1.0.0-myfork/packages/web3/types/index.d.ts"
 import Web3 from 'https://raw.githubusercontent.com/o-ebit/web3/v1.0.0-myfork/packages/web3/src/index.js';
 export default Web3;
